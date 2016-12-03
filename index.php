f3acadf6a18973c8fdee5bf71162fd61c694240a
iShinta <ho_minh_tri@yahoo.fr>
2016-12-02 22:32:29 -0600

    Connection SQL 4

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 
-<html>
-<head>
-</head>
-
-<body>
-  <?php
+<?php
+  function connect(){
     $servername = "fall-2016.cs.utexas.edu";
     $username = "minhtri";
     $password = "EGmf5_qbe1";
@@ -20,53 +16,68 @@
     }
 
     print "Connected to ". mysqli_get_host_info($connect) . "<br /><br />\n";
-  ?>
-  <table>
-    <tr>
-      <th>
-        ID
-      </th>
-      <th>
-        Last
-      </th>
-      <th>
-        First
-      </th>
-      <th>
-        Major
-      </th>
-      <th>
-        GPA
-      </th>
-    </tr>
-    <?php
-        //Get data from table
-        $table = "hwk15_students";
-        $result = mysqli_query($connect, "SELECT * FROM $table");
+  }
 
-        while($row = $result->fetch_row()){
-          ?>
-          <tr>
-            <td>
-              <?php echo $row[0]; ?>
-            </td>
-            <td>
-              <?php echo $row[1]; ?>
-            </td>
-            <td>
-              <?php echo $row[2]; ?>
-            </td>
-            <td>
-              <?php echo $row[3]; ?>
-            </td>
-            <td>
-              <?php echo $row[4]; ?>
-            </td>
-          </tr>
-          <?php
-        }
-        $result->free();
+  function showTable(){
+    connect();
     ?>
-  </table>
+    <table>
+      <tr>
+        <th>
+          ID
+        </th>
+        <th>
+          Last
+        </th>
+        <th>
+          First
+        </th>
+        <th>
+          Major
+        </th>
+        <th>
+          GPA
+        </th>
+      </tr>
+    <?php
+    //Get data from table
+    $table = "hwk15_students";
+    $result = mysqli_query($connect, "SELECT * FROM $table");
+
+    while($row = $result->fetch_row()){
+      ?>
+      <tr>
+        <td>
+          <?php echo $row[0]; ?>
+        </td>
+        <td>
+          <?php echo $row[1]; ?>
+        </td>
+        <td>
+          <?php echo $row[2]; ?>
+        </td>
+        <td>
+          <?php echo $row[3]; ?>
+        </td>
+        <td>
+          <?php echo $row[4]; ?>
+        </td>
+      </tr>
+      <?php
+    }
+    $result->free();
+    ?>
+    </table>
+    <?php
+  }
+?>
+
+
+<html>
+<head>
+</head>
+
+<body>
+  <?php connect(); ?>
 </body>
 </html>
