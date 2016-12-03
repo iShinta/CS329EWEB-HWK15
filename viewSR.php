ade1386967065fc72d7092f663af6d320b913eae
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 00:23:32 -0600

    View 0.1

--- viewSR.php
+++ viewSR.php
@@ -3,58 +3,6 @@
 <?php
 function start() {
   showTable();
-  $host = "fall-2016.cs.utexas.edu";
-  $user = "minhtri";
-  $pwd = "EGmf5_qbe1";
-  $dbs = "cs329e_minhtri";
-  $port = "3306";
-
-  $connect = mysqli_connect ($host, $user, $pwd, $dbs, $port);
-
-  if (empty($connect))
-  {
-    die("mysqli_connect failed: " . mysqli_connect_error());
-  }
-
-  print "Connected to ". mysqli_get_host_info($connect) . "<br /><br />\n";
-
-  $table = "hwk15_students";
-
-
-  $result = mysqli_query($connect, "SELECT * from $table");
-  while ($row = $result->fetch_row())
-  {
-    print "LastName = " . $row[0] . " FirstName = " . $row[1].
-  	" Major = " . $row[2] . " Birthday = " . $row[3] . "<br /><br />\n";
-  }
-
-  $result->free();
-
-  $id = $_POST["ID"];
-  $last = $_POST["LAST"];
-  $first = $_POST["FIRST"];
-  $major = $_POST["MAJOR"];
-  $gpa = $_POST["GPA"];
-
-  if ($id != "" && $last != "" && $first != "" && $major != "" && $gpa != "") {
-    $stmt = mysqli_prepare ($connect, "INSERT INTO $table VALUES (?, ?, ?, ?, ?)");
-    mysqli_stmt_bind_param ($stmt, 'issss', $id, $last, $first, $major, $gpa);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    echo("inserted\n\n");
-
-    $result = mysqli_query($connect, "SELECT * from $table");
-    while ($row = $result->fetch_row())
-    {
-      print "LastName = " . $row[0] . " FirstName = " . $row[1].
-    	" Major = " . $row[2] . " Birthday = " . $row[3] . "<br /><br />\n";
-    }
-
-    $result->free();
-
-  } else {
-    echo("empty field(s)");
-  }
 }
 
 function showTable() {
