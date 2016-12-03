16fb00870d19e92d1fd0f848d17d184912fcfb9a
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 00:56:03 -0600

    View 1.4

--- logoutSR.php
+++ logoutSR.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> LOGGED OUT </title>
+</head>
+<body>
+<h3> LOGGED OUT </h3>
+You have been logged out. <a href=\"index.php\"> Back to the homepage </a>
+
+</body>
+</html>
+
+
 <?php
 setcookie("id", $username, time()-3600);
 setcookie("timeloggedin", time(), time()-3600);
 destroySession();
 ?>
-<a href=\"index.php\"> You have been logged out. Back to the homepage </a>
