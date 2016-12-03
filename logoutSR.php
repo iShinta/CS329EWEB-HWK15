8d1c24c202fb363beda2651843791369c2feeae1
iShinta <ho.minhtri.net@gmail.com>
2016-12-03 00:51:58 -0600

    View 1.3

--- logoutSR.php
+++ logoutSR.php
@@ -3,3 +3,4 @@ setcookie("id", $username, time()-3600);
 setcookie("timeloggedin", time(), time()-3600);
 destroySession();
 ?>
+<a href=\"index.php\"> You have been logged out. Back to the homepage </a>
